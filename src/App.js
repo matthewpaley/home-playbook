3795d7a85a178fe6f11473ec41946bdf7bf3606f
Matthew Paley <matthewpaley@gmail.com>
2024-10-24 17:17:49 -0400

    edited loading page

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <b>
+          <br></br>
+          Owning a home just got easy.
+          <br></br>
+        </b>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/matthewpaley/home-playbook"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
